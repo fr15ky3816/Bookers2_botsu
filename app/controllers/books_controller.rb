525304d7f5da76c39024fafeaae19e47b3274b4c
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-18 10:28:51 +0000

    add 4/18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,32 @@
 class BooksController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+
+
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :caption)
+  end
+
+
 end
