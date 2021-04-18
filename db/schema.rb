5506822a5393c7f7b40e321609e9f2f2df2f960b
fr15ky3816 <shota.t.3816@gmail.com>
2021-04-18 09:02:57 +0000

    add 4/18

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_144811) do
+ActiveRecord::Schema.define(version: 2021_04_18_080222) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
